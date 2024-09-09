d8fbb4bd43045a8dde89b8d395ecddb712ddbba0
RosianeCanella <canella.rosiane@gmail.com>
2024-09-09 14:14:00 -0300

    first commit

---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+    {
+        titulo: "Trade marketing",
+        descricao : "A análise de dados no trade marketing é fundamental para otimizar a presença de produtos no ponto de venda e impulsionar as vendas. Ao coletar e analisar dados sobre desempenho de produtos, comportamento do consumidor, eficácia de promoções e outras métricas relevantes, as empresas podem tomar decisões mais estratégicas e personalizadas. Essa prática permite identificar oportunidades de crescimento, otimizar investimentos, melhorar a gestão de categorias e fortalecer o relacionamento com os parceiros comerciais. Em suma, a análise de dados transforma o trade marketing em uma atividade mais precisa e eficiente, alinhada com os objetivos de negócio.",
+        link: "https://smollanitrade.com/?gad_source=1&gclid=Cj0KCQjwiuC2BhDSARIsALOVfBIZsSF9A8rS2HSLLgW_K_WZWA4zb2zaK-gcZ6pCDirq4ySkGQCSnEcaAm7SEALw_wcB",
+        tags: "Empresa de Trade Marketing"
+    },
+    {
+    titulo: "Análise de Vendas",
+    descricao : "A análise de vendas permite identificar tendências de mercado, produtos mais vendidos, sazonalidade e outros insights importantes para a tomada de decisão. Através da análise dos dados de vendas, é possível otimizar o mix de produtos, ajustar preços e direcionar os esforços de marketing de forma mais eficaz.",
+    link: "https://app.powerbi.com/view?r=eyJrIjoiYTE5NzBjNjgtOTg5YS00MWI5LWExNzktMWFkOGFhNTJlOTcyIiwidCI6IjMzMTMxYTMzLTA5NDAtNGQzYy04NTdjLWJjNjkwNmI2YzY0YSJ9",
+    tags: "Análise de dados de vendas"
+    },
+    {
+        titulo:  "Relatório Contábil - DRE",
+        descricao : "O Demonstrativo do Resultado do Exercício (DRE) é uma ferramenta fundamental para avaliar a saúde financeira da empresa. Através da análise da DRE, é possível identificar as principais fontes de receita, os custos e as despesas, além de calcular o lucro líquido. Essa análise é essencial para a tomada de decisões estratégicas e para o acompanhamento do desempenho da empresa ao longo do tempo.",
+        link: "https://app.powerbi.com/view?r=eyJrIjoiMTEzN2NjOWMtNWUxNi00Mzc4LTg1NmUtMjcxNGZmMWJhZjgwIiwidCI6IjMzMTMxYTMzLTA5NDAtNGQzYy04NTdjLWJjNjkwNmI2YzY0YSJ9",
+        tags: "Contabilidade"
+        }
+];
+
